dd5e21f24b716647bba96c6028b84c9792c2bdde
cravelight <cravelight@gmail.com>
2014-11-16 01:35:00 -0600

    updates to lightswitch

--- lightswitch.php
+++ lightswitch.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+use Carbon\Carbon;
 
 define("SITEROOT", dirname(__FILE__) . '/');
 define("MIGRATIONS", SITEROOT."sql/migrations/");
@@ -11,6 +12,8 @@ define("MIGR_PREFIX", "migr_");
 define("SEED_PREFIX", "seed_");
 define("MIGR_SUFFIX", "_");
 define("SEED_SUFFIX", "_");
+
+define("REPORTING_TIMEZONE", 'America/Chicago');
 
 
 
@@ -77,6 +80,17 @@ class Lightswitch
        $this->history = json_decode(file_get_contents(HISTORY_FILE));
     }
 
+    private function initSeedGroupHistory($group)
+    {
+        if (isset($this->history->seedGroups->$group)) { return; }
+        $this->history->seedGroups->$group = array(
+                        'lastSeed' => $this->defaultTimestamp,
+                        'appliedSeeds' => array()
+                        );
+        $this->saveHistory();
+        $this->loadHistory();
+    }
+
     private function saveHistory()
     {
         file_put_contents(HISTORY_FILE, json_encode($this->history, JSON_PRETTY_PRINT));
@@ -98,8 +112,11 @@ class Lightswitch
         echo "| Run all MIGRATIONS that haven't been run \n";
         echo "|   php $mefile migrate \n";
         echo "| \n";
-        echo "| Run all SEEDS that haven't been run \n";
+        echo "| Run all SEEDS that haven't been run in the default seed group \n";
         echo "|   php $mefile seed \n";
+        echo "| \n";
+        echo "| Run all SEEDS that haven't been run in the given seed group \n";
+        echo "|   php $mefile seed seedGroup \n";
         echo "| \n";
         echo "| Display the STATUS of all migrations and seeds \n";
         echo "|   php $mefile status \n";
@@ -133,7 +150,7 @@ class Lightswitch
                     $this->runMigrations();
                     break;
                 case "seed":
-                    $this->runSeed();
+                    $this->runSeeds();
                     break;
                 case "status":
                     $this->runStatus();
@@ -147,7 +164,7 @@ class Lightswitch
 
         // handle running of named seed group
         if ($this->args[1] == 'seed') {
-            $this->runSeed($this->args[2]);
+            $this->runSeeds($this->args[2]);
             return;
         }
 
@@ -213,22 +230,40 @@ class Lightswitch
         echo "/ - - - - - - - - - - - - - - - - - - - - - - - - -\n";
         echo "| Lightswitch Status  \n";
         echo "| Database: $dbname \n";
+        echo "| Reporting Timezone: ".REPORTING_TIMEZONE." \n";
         echo "| - - - - - - - - - - - - - - - - - - - - - - - - -\n";
         echo "| \n";
-        echo "| - Migrations - - - - -\n";
+        echo "| - Completed Migrations - - - - -\n";
         if ($hasMigratedBefore) {
-            foreach ($array as $key => $value) {
-                
+            foreach ($this->history->appliedMigrations as $migration) {
+                $ranOnAsDateTimeString = Carbon::createFromTimeStamp($migration->ranOn, REPORTING_TIMEZONE)->toDateTimeString();
+                echo "| $ranOnAsDateTimeString $migration->class \n";
             }
         } else {
             echo "| No migrations have been run against this database. \n";
         }
         echo "| \n";
+        echo "| - Open Migrations - - - - -\n";
+        $openMigrations = $this->getMigrationsNotYetRun();
+        if (count($openMigrations) > 0) {
+            foreach ($openMigrations as $migration) {
+                $className = $this->getClassNameFromFilePath($migration);
+                echo "| $className \n";
+            }
+        } else {
+            echo "| All migrations have been run against this database. \n";
+        }
+        echo "| \n";
+        echo "| - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+        echo "| \n";
         foreach ($this->history->seedGroups as $groupName => $groupHistory) {
             $hasSeededBefore = $groupHistory->lastSeed != $this->defaultTimestamp;
             echo "| - $groupName Seeds - - - - -\n";
             if ($hasSeededBefore) {
-
+                foreach ($groupHistory->appliedSeeds as $seed) {
+                    $ranOnAsDateTimeString = Carbon::createFromTimeStamp($seed->ranOn, REPORTING_TIMEZONE)->toDateTimeString();
+                    echo "| $ranOnAsDateTimeString $seed->class \n";
+                }
             } else {
                 echo "| No $groupName seeds have been run against this database. \n";
             }
@@ -237,32 +272,25 @@ class Lightswitch
         echo "\ - - - - - - - - - - - - - - - - - - - - - - - - -\n";
         echo "\n";
         
-        //America/Chicago
 
-    }
-
-
-    private function getCleanName($name)
-    {
-        if (empty($name)) { return ''; }
-        return preg_replace('/[^a-zA-Z0-9]/', '', $name);
     }
 
 
     private function runMigrations()
     {
-        $files = glob(MIGRATIONS.'*.php');
+        $files = $this->getMigrationsNotYetRun();
         foreach ($files as $file) {
             $thisMigrationFailed = true;
             try {
-                // todo: add code to skip/continue if this one is older than lastMigration
-                require_once($file);
-                $className = basename($file, '.php');
-                $migrationId = $this->getIdFromClassName($className);
-                echo $migrationId . "--\n";
+                $className = $this->getClassNameFromFilePath($file);
+                $migrationTimestamp = $this->getTimestampFromClassName($className);
 
+                echo "Running migration $className. \n";
+//                require_once($file);
 //                $obj = new $className;
 //                $obj->run($this->db);
+                echo "Migration $className completed. \n";
+
                 $thisMigrationFailed = false;
             } catch (\Exception $exc) {
                 echo "Migration $className failed. \n";
@@ -270,7 +298,7 @@ class Lightswitch
                 break;
             } finally {
                 if ($thisMigrationFailed) { return; }
-                $this->history->lastMigration = $migrationId;
+                $this->history->lastMigration = $migrationTimestamp;
                 $this->history->appliedMigrations[] = array(
                     'ranOn' => time(),
                     'class' => $className
@@ -280,39 +308,85 @@ class Lightswitch
         }
     }
 
-    private function getIdFromClassName($className)
+
+    private function runSeeds($group = 'default')
+    {
+        $this->initSeedGroupHistory($group);
+        $files = $this->getSeedsNotYetRunForGroup($group);
+        var_dump($files);
+        $seedGroupHistory = $this->history->seedGroups->$group;
+        foreach ($files as $file) {
+            $thisSeedFailed = true;
+            try {
+                $className = $this->getClassNameFromFilePath($file);
+                $seedTimestamp = $this->getTimestampFromClassName($className);
+
+                echo "Running seed $className. \n";
+//                require_once($file);
+//                $obj = new $className;
+//                $obj->run($this->db);
+                echo "Seed $className completed. \n";
+
+                $thisSeedFailed = false;
+            } catch (\Exception $exc) {
+                echo "Seed $className failed. \n";
+                echo $exc->getTraceAsString()."\n";
+                break;
+            } finally {
+                if ($thisSeedFailed) { return; }
+                $seedGroupHistory->lastSeed = $seedTimestamp;
+                $seedGroupHistory->appliedSeeds[] = array(
+                    'ranOn' => time(),
+                    'class' => $className
+                    );
+                $this->saveHistory();
+            }
+        }
+    }
+
+
+    private function getCleanName($name)
+    {
+        if (empty($name)) { return ''; }
+        return preg_replace('/[^a-zA-Z0-9]/', '', $name);
+    }
+
+    private function getMigrationsNotYetRun()
+    {
+        $files = glob(MIGRATIONS.'*.php');
+        $notRun = array_filter($files, function($file) //use($NUM)
+            {
+                $className = $this->getClassNameFromFilePath($file);
+                $migrationTimestamp = $this->getTimestampFromClassName($className);
+                return $migrationTimestamp > $this->history->lastMigration;
+            });
+        return $notRun;
+    }
+
+    private function getSeedsNotYetRunForGroup($group)
+    {
+        $files = glob(SEEDS.$group.'/*.php');
+        $seedGroupHistory = $this->history->seedGroups->$group;
+        $notRun = array_filter($files, function($file) use($seedGroupHistory)
+            {
+                $className = $this->getClassNameFromFilePath($file);
+                $seedTimestamp = $this->getTimestampFromClassName($className);
+                return $seedTimestamp > $seedGroupHistory->lastSeed;
+            });
+        return $notRun;
+    }
+
+
+    private function getClassNameFromFilePath($filePath)
+    {
+        return basename($filePath, '.php');
+    }
+
+    private function getTimestampFromClassName($className)
     {
         return split('_', $className)[1];
     }
 
-    private function runSeed($group = 'default')
-    {
-        $files = glob(SEEDS.$group.'/*.php');
-        $this->run($files);
-    }
-
-
-    private function run($files)
-    {
-        foreach ($files as $file) {
-            try {
-
-
-                echo $file."\n";
-
-                require_once($file);
-                $class = basename($file, '.php');
-                echo $class."\n";
-
-//                $obj = new $class;
-//                $obj->run($this->db);
-
-            } catch (\Exception $exc) {
-                echo $exc->getTraceAsString();
-                break;
-            }
-        }
-    }
 }
 
 $lightswitch = new Lightswitch($app->db, $argv);
